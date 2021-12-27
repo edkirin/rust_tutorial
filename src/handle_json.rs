a915f302f31af25f6fc140927fc053380615b218
Eden Kirin <eden@ekirin.com>
2021-12-27 12:22:06 +0100

    handle_json

--- src/handle_json.rs
+++ src/handle_json.rs
@@ -1,43 +1,50 @@
 use std::fs::File;
 use std::io::prelude::*;
+use serde::{Deserialize, Serialize};
+// use serde_json::Result;
 
 
-struct PersonInSpace {
+#[derive(Serialize, Deserialize)]
+struct Person {
     craft: String,
     name: String,
 }
 
+#[derive(Serialize, Deserialize)]
 struct PeopleInSpace {
     message: String,
     number: i32,
-    people: Vec<PersonInSpace>,
+    people: Vec<Person>,
 }
 
 
-impl PeopleInSpace {
-    pub fn read_from_file(&self, filename: &str) -> std::io::Result<()> {
-        let mut file = File::open(filename);
-        let mut contents = String::new();
-        file.read_to_string(&mut contents);
-        return contents;
-    }
+fn read_from_file(filename: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(filename)?;
+    let mut data = String::new();
+    file.read_to_string(&mut data);
+    Ok(data)
 }
 
 
-// fn read_people_in_space_from_file() -> std::io::Result<()> {
-//     const DATA_FILE: &str = "demodata/people-in-space.json";
-//     println!("Reading file {}", DATA_FILE);
-//
-//     let mut file = File::open(DATA_FILE)?;
-//     let mut contents = String::new();
-//     file.read_to_string(&mut contents)?;
-//     println!("{}", contents);
-//
-//     Ok(())
-// }
+fn create_from_json(json_data: &str) -> PeopleInSpace {
+    println!("{}", json_data);
+
+    let pis: PeopleInSpace = serde_json::from_str(json_data).unwrap();
+    pis
+}
 
 
 pub fn handle_json_demo() {
-    let pis = PeopleInSpace::new();
-    pis.read_from_file("demodata/people-in-space.json");
+    let json_data: String =
+        read_from_file("demodata/people-in-space.json")
+            .expect("Error reading file!"); // FIXME: error catching not working;
+
+    let pis: PeopleInSpace = create_from_json(&json_data);
+
+    println!("{}", pis.number);
+    println!("{}", pis.message);
+
+    for person in pis.people {
+        println!("{}: {}", person.craft, person.name);
+    }
 }
